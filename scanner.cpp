4be7104c1481ba9acfe06be03c8d3f192a0f961d
agamyo168 <70448818+agamyo168@users.noreply.github.com>
2021-11-09 01:42:11 +0200

    added scanner

--- scanner.cpp
+++ scanner.cpp
@@ -1,2 +1,186 @@
 #include "scanner.h"
+using namespace std;
+Token::Token(string strValue = "",string type = "")
+{
+    this->strValue = strValue;
+    this->type = type;
+}
+string lowerCase(string x)
+{
+    string result;
+    for(auto i = x.begin(); i!=x.end(); ++i)
+    {
+        result += tolower(*i);
+    }
+    return result;
+}
+string idRes (string strValue)
+{
+    if(reservedWords.find(lowerCase(strValue)) != string::npos) return "reserved word";
+    return "identifier";
+}
+string symbolType(char c)
+{
+     switch(c)
+    {
+        case '+': return "plus";
+        case '-': return "minus";
+        case '*': return "times";
+        case '/': return "over";
+        case '=': return "equal";
+        case '<': return "less than";
+        case '(': return "left parenthesis";
+        case ')': return "right parenthesis";
+        case ';': return "semicolon";
+    }
+}
+string Scanner(string TinyFileText)
+{
+    vector<Token> tokenList; // List of tokens
+    string tokenTable;
+    int index = 0;
+    int length = TinyFileText.length();
+    //Loops over the string
+        while(index<length)
+        {
+            int state = 1;
+            string strValue = "";
+
+            //Checks for a token
+            //Basically, this is getToken()
+            while (state != 6&& index<length)
+            {
+
+                switch(state)
+                {
+                    case 1:
+
+                    if(TinyFileText[index] == '{')
+                        {
+                            state=2;
+                            index++;
+                        }
+                    else if(whitespace.find(TinyFileText[index])!= string::npos)
+                        {
+                            index++;
+                            }
+                    else if (isdigit(TinyFileText[index]))
+                        {   strValue += TinyFileText[index];
+                            state=3;
+                            index++;
+                            }
+                    else if (isalpha(TinyFileText[index]))
+                        {   strValue += TinyFileText[index];
+                            state=4;
+                            index++;
+                            }
+                    else if (TinyFileText[index] == ':')
+                        {   strValue += TinyFileText[index];
+                            state=5;
+                            index++;
+                            }
+                    else if(specialSymbols.find(TinyFileText[index]) != string::npos)
+                    {   strValue+=TinyFileText[index];
+                        Token temp(strValue,symbolType(TinyFileText[index]));
+                        tokenList.push_back(temp);
+                        index++;
+                        state = 6;
+
+                    }
+                    else
+                        {   strValue+=TinyFileText[index];
+                            Token temp(strValue,"error");
+                            tokenList.push_back(temp);
+                            index++;
+                            state=6;
+                            }
+                    break;
+                    case 2:
+
+                    while(TinyFileText[index] != '}')
+                        {
+
+                            index++;
+
+                        }
+                        index++;
+                        state=1;
+                    break;
+                    case 3:
+
+                    if(isdigit(TinyFileText[index]))
+                        {
+                            strValue += TinyFileText[index];
+                            index++;
+                        }
+                    else {
+                        Token temp(strValue,"number");
+                        tokenList.push_back(temp);
+                        state = 6;
+                    }
+                    break;
+                    case 4:
+
+                    //Variables can have digits as long as it comes after a letter
+                    if(isalpha(TinyFileText[index])||isdigit(TinyFileText[index]))
+                    {
+                        strValue += TinyFileText[index];
+                        index++;
+                    }
+                    else
+                    {
+                        Token temp(strValue,idRes(strValue));
+                        tokenList.push_back(temp);
+                        state = 6;
+                    }
+                    break;
+                    case 5:
+
+                    if(TinyFileText[index] == '=')
+                        {   strValue+= TinyFileText[index];
+                            index++;
+                            Token temp(strValue,"assign");
+                            tokenList.push_back(temp);
+                            state = 6;
+                        }
+                    else
+                    {
+                        Token temp(strValue,"error");
+                        tokenList.push_back(temp);
+                        state = 6;
+                    }
+                    break;
+                }
+
+            }
+
+
+
+}
+//Output string
+   for(auto i = tokenList.begin();i !=tokenList.end(); ++i)
+    {
+
+      tokenTable+=(*i).strValue+", "+(*i).type+"\n";
+    }
+
+    return tokenTable;
+}
+void testScanner()
+{
+    string line;
+    string fileText;
+    ifstream myfile ("tinyTestError.txt");
+    if (myfile.is_open())
+    {
+    while ( getline (myfile,line) )
+    {
+        fileText+=line+"\n";
+    }
+    string tokenTable = Scanner(fileText);
+    cout<<tokenTable;
+
+    }
+    else cout << "Unable to open file";
+}
 
--- scanner.h
+++ scanner.h
@@ -1,7 +1,26 @@
 #ifndef SCANNER_H
 #define SCANNER_H
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+using namespace std;
+class Token
+{
+public:
+string strValue = "";
+string type ="";
+Token(string strValue, string type);
 
-
-
+};
+static string whitespace = " \n\t\r\v";
+static string reservedWords = "if then else end repeat until read write begin int main string real";
+static string specialSymbols = "+-*/=<();";
+string lowerCase(string x);
+string idRes (string strValue);
+string symbolType(char c);
+string Scanner(string TinyFileText);
+void testScanner();
 
 #endif // SCANNER_H
